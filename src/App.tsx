c2511db1970aeda601a6807008910b750e0acc59
Emerson Shatouhy <emersont2003@gmail.com>
2023-10-31 22:27:10 -0400

    Navbar (#5)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Navbar from "./compontents/navbar/navbar.tsx";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Navbar />
     </>
   );
 }
